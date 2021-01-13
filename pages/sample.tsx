2642c643f79e89e823fbc21a6618f51e5e874ab5
koji-koji <k.koyama37@gmail.com>
2021-01-13 21:17:50 +0900

    :sparkles: サンプルのtext-color設定

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,11 +1,12 @@
 // ファイル名から自動でルーティングが設定される。この場合は/sampleでマッチ。
 // urlになるので、ファイル名はケバブケースが無難。コンポーネント名はキャメルケースなので注意。
-const Sample = () => {
+const Sample: React.FC = () => {
   return (
     <>
+      <div className="p-10 text-appSampleColor">aaa</div>
       SamplePageComponent
     </>
   );
-}
+};
 
 export default Sample;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./pages/**/*.ts', './pages/**/*.tsx', './components/**/*.ts', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      // ここに色を設定すると、classNameにtext-appBaseColorのように記載するだけで文字が赤くなる。
+      // cssに記載するときはbackground-color: theme('colors.orange.200')のように記述する。
+      colors: {
+        appSampleColor: '#de3618',
+        // classNameをtext-gry-liaghterのように記述して設定できる。
+        gray: {
+          lighter: '#aaa',
+          default: '#777',
+          dark: '#444',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
