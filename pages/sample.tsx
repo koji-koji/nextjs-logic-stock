30366087461fbe37fecaab786923034934a241f4
koji-koji <k.koyama37@gmail.com>
2021-01-15 21:28:39 +0900

    :sparkles: サンプルコンポーネントの追加。

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,10 +1,22 @@
+import React from 'react';
+import SimpleTable from '../components/mass/SimpleTable';
+import SortTable from '../components/mass/SortTable';
+import FileFormUPloadArea from '../components/layouts/FileFormUploadArea';
+
 // ファイル名から自動でルーティングが設定される。この場合は/sampleでマッチ。
 // urlになるので、ファイル名はケバブケースが無難。コンポーネント名はキャメルケースなので注意。
 const Sample: React.FC = () => {
   return (
     <>
       <div className="p-10 text-appSampleColor">aaa</div>
-      SamplePageComponent
+      sample_page_component
+      <div className="bg-gray-100">
+        <div className="p-3">
+          <SimpleTable></SimpleTable>
+          <SortTable></SortTable>
+          <FileFormUPloadArea></FileFormUPloadArea>
+        </div>
+      </div>
     </>
   );
 };
